acb6c9a4217b16965c9ab6d15cbd12ea83e2baf2
Jason Awbrey <jawbrey@gmail.com>
2021-04-19 12:17:32 -0500

    Next concert adjustments
    change map button on Next Concert card when concert is virtual

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,52 +1,75 @@
-import React, { Component } from 'react';
-import RouteTransitionTemplate from '../../RouteTransitionTemplate';
-import { presets } from 'react-router-transition';
-import { SoundPlayerContainer } from 'react-soundplayer/addons';
-import { PlayButton } from 'react-soundplayer/components';
+import React, { Component } from "react";
+import RouteTransitionTemplate from "../../RouteTransitionTemplate";
+import { presets } from "react-router-transition";
+import { SoundPlayerContainer } from "react-soundplayer/addons";
+import { PlayButton } from "react-soundplayer/components";
 
-import './home.css';
-import '../../App.css';
+import "./home.css";
+import "../../App.css";
 
-const clientId = 'b66c8aeecc97de91f3ed3fa8b09d8d8c';
+const clientId = "b66c8aeecc97de91f3ed3fa8b09d8d8c";
 
 class Home extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      date: []
+    this.state = {
+      date: [],
+      track: [],
     };
   }
 
-  componentDidMount(){
-    fetch('https://cdn.contentful.com/spaces/lfibhjwf8l76/entries?access_token=ac626e994db36e47fed21fc5d5b5c2019df79ac53a77d06ee13b3163ef7af3a0&content_type=organization&order=fields.timestamp')
-    .then((response) => response.json())
-    .then((responseData) => {
-      let items = responseData.items;
-      let date_sort_asc = (date1, date2) => {
-        let dt1 = new Date(date1);
-        let dt2 = new Date(date2);
-        if (dt1 > dt2) return 1;
-        if (dt1 < dt2) return -1;
-        return 0;
-      };
+  componentDidMount() {
+    fetch(
+      "https://cdn.contentful.com/spaces/lfibhjwf8l76/entries?access_token=ac626e994db36e47fed21fc5d5b5c2019df79ac53a77d06ee13b3163ef7af3a0&content_type=organization&order=fields.timestamp"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        let items = responseData.items;
+        let date_sort_asc = (date1, date2) => {
+          let dt1 = new Date(date1);
+          let dt2 = new Date(date2);
+          if (dt1 > dt2) return 1;
+          if (dt1 < dt2) return -1;
+          return 0;
+        };
 
-      items.sort(date_sort_asc);
+        items.sort(date_sort_asc);
 
-      let concert = null;
+        let concert = null;
 
-      for (var i = 0; i < items.length; i++) {
-        let cDate = new Date(items[i].fields.timestamp);
-        const today = new Date();
-        if (cDate > today) {
-          concert = items[i].fields;
-          break;
+        for (var i = 0; i < items.length; i++) {
+          let cDate = new Date(items[i].fields.timestamp);
+          const today = new Date();
+          if (cDate > today) {
+            concert = items[i].fields;
+            break;
+          }
         }
-      }
-      this.setState({date: concert});
-    })
-    .catch((error) => {
-      console.log('Error fetching and parsing data', error);
-    });
+        this.setState({ date: concert });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+
+    fetch(
+      "https://cdn.contentful.com/spaces/lfibhjwf8l76/entries?access_token=ac626e994db36e47fed21fc5d5b5c2019df79ac53a77d06ee13b3163ef7af3a0&content_type=audio"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        let items = responseData.items;
+        let track = null;
+
+        for (var i = 0; i < items.length; i++) {
+          if (items[i].fields.hpTrack === true) {
+            track = items[i].fields;
+            break;
+          }
+        }
+        this.setState({ track: track });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
   }
 
   render() {
@@ -55,15 +78,19 @@ class Home extends Component {
         <div className="page">
           <section className="hero group">
             <div className="container">
-              <img className="hero_img" src="./images/ja-bg.jpg" alt="Jason Awbrey" />
+              <img
+                className="hero_img"
+                src="./images/ja-bg.jpg"
+                alt="Jason Awbrey"
+              />
             </div>
           </section>
 
           <section className="callouts group">
             <div className="container">
               <NextConcert feed={this.state.date} />
-              <FeaturedAudio/>
-              <RecentMentions/>
+              <FeaturedAudio />
+              <RecentMentions />
             </div>
           </section>
         </div>
@@ -76,35 +103,59 @@ class NextConcert extends Component {
   render() {
     const perf = this.props.feed;
     return (
-        <div className="gridBox gridBox__meyerson-full">
-          <div className="gridBox-overlay">
-            <h2 className="title">Next<br />Concert</h2>
-          
-            
+      <div className="gridBox gridBox__meyerson-full">
+        <div className="gridBox-overlay">
+          <h2 className="title">
+            Next
+            <br />
+            Concert
+          </h2>
+
           {this.props.feed.length === 0 ? (
             <div>
               <p>
                 <i>Check back soon for upcoming events.</i>
               </p>
               <div className="cta__container">
-                <a href="/schedule" target="_blank" className="cta-button button__light">Past Events</a>
+                <a
+                  href="/schedule"
+                  target="_blank"
+                  className="cta-button button__light"
+                >
+                  Past Events
+                </a>
               </div>
             </div>
-            ) : (
-              <div>
-                <p className="gridBox__p">
-                  <i>{perf.date} @ {perf.time}</i><br />
-                  {perf.title} <br />
-                  {perf.organization}
-                </p>
-                <div className="cta__container">
-                  <a href={perf.map} target="_blank" className="cta-button button__light">View Map</a>
-                </div>
+          ) : (
+            <div>
+              <p className="gridBox__p">
+                <i>
+                  {perf.date} @ {perf.time}
+                </i>
+                <br />
+                {perf.title} <br />
+                {perf.organization}
+              </p>
+              <div className="cta__container">
+                {perf.map ? (
+                  <a
+                    href={perf.map}
+                    target="_blank"
+                    className="cta-button button__light"
+                  >
+                    View Map
+                  </a>
+                ) : (
+                  <a href="/schedule" className="cta-button button__light">
+                    See Schedule
+                  </a>
+                )}
               </div>
-            )
-          }
-          </div>
-        </div>);
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
@@ -113,14 +164,23 @@ class RecentMentions extends Component {
     return (
       <div className="gridBox gridBox__messiah">
         <div className="gridBox-overlay">
-          <h2 className="title">Recent<br />Mentions</h2>
-          <blockquote className="blockquote__recent">Elegantly <br />commanding</blockquote>
+          <h2 className="title">
+            Recent
+            <br />
+            Mentions
+          </h2>
+          <blockquote className="blockquote__recent">
+            Elegantly <br />
+            commanding
+          </blockquote>
           <div className="cta__container">
-            <a href="/about" className="cta-button button__light">See More</a>
+            <a href="/about" className="cta-button button__light">
+              See More
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -129,11 +189,15 @@ class FeaturedAudio extends Component {
     return (
       <div className="gridBox gridBox__neruda recent__audio">
         <div className="gridBox-overlay">
-          <h2 className="title">Featured<br />Audio</h2>
+          <h2 className="title">
+            Featured
+            <br />
+            Audio
+          </h2>
           <PlayerContainer />
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -141,38 +205,48 @@ class PlayerContainer extends Component {
   render() {
     return (
       <div>
-        <SoundPlayerContainer streamUrl='https://api.soundcloud.com/tracks/100312444/stream' clientId={clientId} className='group'>
+        <SoundPlayerContainer
+          streamUrl="https://api.soundcloud.com/tracks/100312444/stream"
+          clientId={clientId}
+          className="group"
+        >
           <Player />
         </SoundPlayerContainer>
       </div>
-    )
-    
+    );
   }
 }
 
 class Player extends Component {
   stopPlay() {
-      let { playing, soundCloudAudio } = this.props;
-      if (playing) {
-          soundCloudAudio.stop();
-      }
+    let { playing, soundCloudAudio } = this.props;
+    if (playing) {
+      soundCloudAudio.stop();
+    }
   }
-    render() {
-        return (
-            <div className="">
-              <div className="flex-auto">
-                <p className="audio__title">A Spotless Rose<br />
-                  <span className="audio__composer"><i>Herbert Howells</i></span>
-                </p>
-              </div>
-              <PlayButton className="button black btn-big button-outline button-grow bg-white circle" {...this.props} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="">
+        <div className="flex-auto">
+          <p className="audio__title">
+            A Spotless Rose
+            <br />
+            <span className="audio__composer">
+              <i>Herbert Howells</i>
+            </span>
+          </p>
+        </div>
+        <PlayButton
+          className="button black btn-big button-outline button-grow bg-white circle"
+          {...this.props}
+        />
+      </div>
+    );
+  }
 
-    componentWillUnmount() {
-      this.stopPlay();
-    }
+  componentWillUnmount() {
+    this.stopPlay();
+  }
 }
 
 export default Home;
